86ffd3fe93bb8e8b9731438950d467230c8f81ad
Goutham C M <goutham.898@gmail.com>
2021-11-01 10:05:52 +0530

    Added 404

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+  
+// import { motion } from 'framer-motion';
+// import { motion } from "framer-motion"
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Button, Typography, Container } from '@mui/material';
+// components
+
+
+  
 function PageNotFound() {
     return (
         <div>
-            404
-        </div>
+      
+        
+          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
+            
+              <Typography variant="h3" paragraph>
+                Sorry, page not found!
+              </Typography>
+            
+            <Typography sx={{ color: 'text.secondary' }}>
+              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL?
+              Be sure to check your spelling.
+            </Typography>
+      
+            
+          </Box>
+        
+      
+    </div>
     )
 }
 
